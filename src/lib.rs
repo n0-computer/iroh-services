417fa8abb81a718e0e12a616dd8345be48cecf16
b5 <sparkle_pony_2000@qri.io>
2026-02-05 19:24:05 -0500

    refactor!: remove n0des & simulation interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -28,36 +28,19 @@
 //! [n0des]: https://n0des.iroh.computer
 //! [iroh]: https://iroh.computer
 mod client;
-mod n0des;
 
 pub mod api_secret;
 pub mod caps;
 pub mod protocol;
-#[cfg(feature = "simulation")]
-pub mod simulation;
-
-pub use iroh_n0des_macro::sim;
-
-// This lets us use the derive metrics in the lib tests within this crate.
-extern crate self as iroh_n0des;
 
 pub use anyhow;
 pub use iroh_metrics::Registry;
 
 #[cfg(feature = "tickets")]
 pub use self::client::PublishedTicket;
+
 pub use self::{
     api_secret::ApiSecret,
     client::{Client, ClientBuilder},
-    n0des::N0de,
     protocol::ALPN,
 };
-
-#[cfg(test)]
-mod tests {
-    // keep to make sure that there is a test even with all features disabled.
-    #[test]
-    fn dummy() {
-        assert_eq!(2 + 2, 4);
-    }
-}
