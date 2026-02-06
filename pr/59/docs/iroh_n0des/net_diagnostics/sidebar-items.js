8727685139d307aa72a6f713777a768eabca5a40
b5 <b5@users.noreply.github.com>
2026-02-06 00:03:58 +0000

    deploy: 55996fba5d945c77e1a55ea831b93c6539441854

--- pr/59/docs/iroh_n0des/net_diagnostics/sidebar-items.js
+++ pr/59/docs/iroh_n0des/net_diagnostics/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["diagnose"],"struct":["DiagnosticsReport"]};
+window.SIDEBAR_ITEMS = {"fn":["run_diagnostics"],"struct":["DiagnosticsReport"]};
---
+++ pr/59/docs/trait.impl/iroh/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["iroh_n0des",[["impl ProtocolHandler for <a class=\"struct\" href=\"iroh_n0des/protocol/client_host/struct.ClientHost.html\" title=\"struct iroh_n0des::protocol::client_host::ClientHost\">ClientHost</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[209]}
