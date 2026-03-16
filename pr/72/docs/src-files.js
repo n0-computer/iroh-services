10810f6ffba198352c9099e2239a26a93b965bd7
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-03-16 18:42:51 +0000

    deploy: ec1f74b3dfeda05baf2b6917fd7a586412073baa

---
+++ pr/72/docs/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["iroh_services",["",[],["api_secret.rs","caps.rs","client.rs","client_host.rs","lib.rs","net_diagnostics.rs","protocol.rs"]]]]');
+//{"start":19,"fragment_lengths":[126]}
