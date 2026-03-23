c470d70ec147bd11432de7a1c800494e18f908bb
b5 <b5@users.noreply.github.com>
2026-03-23 19:56:53 +0000

    deploy: e33b26e11286fef40f9400c9c3c0fd2e3e9f8bbb

---
+++ pr/74/docs/iroh_services/protocol/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["ALPN"],"enum":["ClientHostProtocol","IrohServicesProtocol","NetDiagnosticsMessage","RemoteError","ServicesMessage"],"struct":["Auth","GrantCap","Ping","Pong","PutMetrics","PutNetworkDiagnostics","RunNetworkDiagnostics"],"type":["IrohServicesClient","RemoteResult"]};
