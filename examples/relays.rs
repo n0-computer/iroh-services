0667e1cbdb765925b7ae00d91c5e0decf4dc4462
b5 <sparkle_pony_2000@qri.io>
2026-06-01 16:30:44 -0400

    improve relays example

--- examples/relays.rs
+++ examples/relays.rs
@@ -13,33 +13,36 @@ use iroh::{Endpoint, SecretKey};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     // minimal preset that works with a free iroh services project. This will
     // give a bandwidth bump when using the public relays, and surface your
     // relay traffic on your project:
     let _preset = iroh_services::preset().api_secret_from_env()?.build()?;
 
-    // pro & enterprise projects have access to custom relays, which are set
-    // with the `relays` method on the builder.
-    // You'll need to replace these strings with the relay urls for your project.
-    let _preset = iroh_services::preset()
-        .relays([
-            "https://use1-1.relay.username.project.iroh.link",
-            "https://usw1-1.relay.username.project.iroh.link",
-            "https://euc1-1.relay.username.project.iroh.link",
-        ])?
-        .api_secret_from_env()?
-        .build()?;
-
     // if you are using a secret key from disk, or generally want control over
     // the ID your endpoint uses, the secret key must be given to the preset
-    // before passing it to the endpoint. The access token that the preset creates
+    // *before* passing it to the endpoint. The access token that the preset creates
     // to access relays is scoped only to the given key.
     //
     // If no key is provided, a random one is generated and passed to the
     // endpoint.
     let secret_key = SecretKey::generate();
-    let preset = iroh_services::preset()
+    let _preset = iroh_services::preset()
         .secret_key(secret_key)
+        .api_secret_from_env()?
+        .build()?;
+
+    // pro & enterprise projects have access to custom relays, which are set
+    // with the `relays` method on the builder.
+    // You'll need to replace these strings with the relay urls for your project
+    let preset = iroh_services::preset()
+        .relays([
+            // Your Relay URLS here. eg:
+            "https://use1-1.relay.username.project.iroh.link",
+            "https://usw1-1.relay.username.project.iroh.link",
+            "https://euc1-1.relay.username.project.iroh.link",
+        ])?
         .api_secret_from_env()?
         .build()?;
 
@@ -58,6 +61,12 @@ async fn main() -> anyhow::Result<()> {
 
     // we can also ping the service just to confirm everything is working
     client.ping().await?;
+
+    // keep the connection alive
+    println!("waiting for ctrl+c...");
+    tokio::signal::ctrl_c().await?;
+
+    endpoint.close().await;
 
     Ok(())
 }
