d34d01f1217cad6af97186c0d72c16fb939c05cc
b5 <sparkly_pony_2000@qri.io>
2026-06-04 15:27:27 -0400

    fix: use only hardcoded values for easier example modification

--- examples/relays.rs
+++ examples/relays.rs
@@ -9,7 +9,7 @@
 //! Your app will use only one of these methods, depending on your use case. To test this
 //! example with custom relay URLS, you will need to comment out the secret key preset
 //! example and use the `relays` method instead, pasting in your own relay URLs.
-use iroh::{Endpoint, SecretKey};
+use iroh::Endpoint;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,7 +18,9 @@ async fn main() -> anyhow::Result<()> {
     // minimal preset that works with a free iroh services project. This will
     // give a bandwidth bump when using the public relays, and surface your
     // relay traffic on your project:
-    let _preset = iroh_services::preset().api_secret_from_env()?.build()?;
+    // let _preset = iroh_services::preset()
+    //     .api_secret_from_str("YOUR_API_SECRET_HERE")?
+    //     .build()?;
 
     // if you are using a secret key from disk, or generally want control over
     // the ID your endpoint uses, the secret key must be given to the preset
@@ -27,23 +29,25 @@ async fn main() -> anyhow::Result<()> {
     //
     // If no key is provided, a random one is generated and passed to the
     // endpoint.
-    let secret_key = SecretKey::generate();
-    let _preset = iroh_services::preset()
-        .secret_key(secret_key)
-        .api_secret_from_env()?
-        .build()?;
+    // let secret_key = iroh::SecretKey::generate();
+    // let _preset = iroh_services::preset()
+    //     .secret_key(secret_key)
+    //     .api_secret_from_str("YOUR_API_SECRET_HERE")?
+    //     .build()?;
 
     // pro & enterprise projects have access to custom relays, which are set
     // with the `relays` method on the builder.
-    // You'll need to replace these strings with the relay urls for your project
+    // You'll need to replace these strings with the relay urls for your project,
+    // and set the API secret.
     let preset = iroh_services::preset()
         .relays([
-            // Your Relay URLS here. eg:
-            "https://use1-1.relay.username.project.iroh.link",
-            "https://usw1-1.relay.username.project.iroh.link",
-            "https://euc1-1.relay.username.project.iroh.link",
+            // Replace these with your own relay urls!
+            "https://use1-1.relay.n0.iroh-canary.iroh.link.",
+            "https://usw1-1.relay.n0.iroh-canary.iroh.link.",
+            "https://euc1-1.relay.n0.iroh-canary.iroh.link.",
+            "https://aps1-1.relay.n0.iroh-canary.iroh.link.",
         ])?
-        .api_secret_from_env()?
+        .api_secret_from_str("YOUR_API_SECRET_HERE")?
         .build()?;
 
     // once a preset is built, we'll pass it to the endpoint for binding.
