d5dda9d0e9fd9b1388d1f2dfceac1d1bb2837fe1
Frando <frando@unbiskant.org>
2026-07-24 10:33:36 +0200

    example with relays from env

--- examples/relays.rs
+++ examples/relays.rs
@@ -9,50 +9,67 @@
 //! Your app will use only one of these methods, depending on your use case. To test this
 //! example with custom relay URLS, you will need to comment out the secret key preset
 //! example and use the `relays` method instead, pasting in your own relay URLs.
-use iroh::Endpoint;
+use std::str::FromStr;
+
+use anyhow::Context;
+use clap::Parser;
+use data_encoding::HEXLOWER;
+use iroh::{Endpoint, EndpointId, RelayMap, RelayUrl, SecretKey, protocol::Router};
+
+fn relay_map_from_env() -> anyhow::Result<Option<RelayMap>> {
+    match std::env::var("IROH_RELAYS") {
+        Ok(value) if !value.is_empty() => {
+            let parts = value.split(",");
+            let mut urls = Vec::new();
+            for url_string in parts {
+                let url: RelayUrl = url_string
+                    .trim()
+                    .parse()
+                    .with_context(|| "Failed to parse string as relay URL: {url_string}")?;
+                urls.push(url);
+            }
+            Ok(Some(RelayMap::from_iter(urls)))
+        }
+        _ => Ok(None),
+    }
+}
+
+#[derive(Debug, clap::Parser)]
+struct Cli {
+    targets: Vec<EndpointId>,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    // minimal preset that works with a free iroh services project. This will
-    // give a bandwidth bump when using the public relays, and surface your
-    // relay traffic on your project:
-    // let _preset = iroh_services::preset()
-    //     .api_secret_from_str("YOUR_API_SECRET_HERE")?
-    //     .build()?;
+    let cli = Cli::parse();
 
-    // if you are using a secret key from disk, or generally want control over
-    // the ID your endpoint uses, the secret key must be given to the preset
-    // *before* passing it to the endpoint. The access token that the preset creates
-    // to access relays is scoped only to the given key.
-    //
-    // If no key is provided, a random one is generated and passed to the
-    // endpoint.
-    // let secret_key = iroh::SecretKey::generate();
-    // let _preset = iroh_services::preset()
-    //     .secret_key(secret_key)
-    //     .api_secret_from_str("YOUR_API_SECRET_HERE")?
-    //     .build()?;
+    // Create secret key if not set.
+    let secret_key = match std::env::var("IROH_SECRET") {
+        Ok(s) => SecretKey::from_str(&s)
+            .context("Failed to parse IROH_SECRET environment variable as iroh secret key")?,
+        Err(_) => {
+            let s = SecretKey::generate();
+            println!(
+                "Generated a new endpoint secret. To reuse, set\n\tIROH_SECRET={}",
+                HEXLOWER.encode(&s.to_bytes())
+            );
+            s
+        }
+    };
 
-    // pro & enterprise projects have access to custom relays, which are set
-    // with the `relays` method on the builder.
-    // You'll need to replace these strings with the relay urls for your project,
-    // and set the API secret.
+    let relay_map = relay_map_from_env()?.context("Missing IROH_RELAYS environment variable")?;
+
     let preset = iroh_services::preset()
-        .relays([
-            // Replace these with your own relay urls!
-            "https://use1-1.relay.n0.iroh-canary.iroh.link.",
-            "https://usw1-1.relay.n0.iroh-canary.iroh.link.",
-            "https://euc1-1.relay.n0.iroh-canary.iroh.link.",
-            "https://aps1-1.relay.n0.iroh-canary.iroh.link.",
-        ])?
-        .api_secret_from_str("YOUR_API_SECRET_HERE")?
+        .secret_key(secret_key)
+        .relay_map(relay_map)
+        .api_secret_from_env()?
         .build()?;
 
     // once a preset is built, we'll pass it to the endpoint for binding.
     // we clone the preset so we can reuse to get a client builder below
-    let endpoint = Endpoint::bind(preset.clone()).await?;
+    let endpoint = Endpoint::builder(preset.clone()).bind().await?;
 
     // wait for the endpoint to be online, to prove we have an authorized
     // connection to a relay
@@ -63,14 +80,30 @@ async fn main() -> anyhow::Result<()> {
     // endpoint, so you don't need to pass the secret key separately:
     let client = preset.client_builder(&endpoint).build().await?;
 
+    println!("endpoint id: {}", endpoint.id());
+    let router = Router::builder(endpoint)
+        .accept(iroh_ping::ALPN, iroh_ping::Ping::default())
+        .spawn();
+
     // we can also ping the service just to confirm everything is working
-    client.ping().await?;
+    tokio::task::spawn(async move {
+        let res = client.ping().await;
+        println!("Pinged iroh services: {res:?}");
+    });
+
+    for target in cli.targets {
+        println!("ping {target}...");
+        let res = iroh_ping::Ping::new()
+            .ping(router.endpoint(), target.into())
+            .await;
+        println!("ping {target}: {res:?}");
+    }
 
     // keep the connection alive
     println!("waiting for ctrl+c...");
     tokio::signal::ctrl_c().await?;
 
-    endpoint.close().await;
+    router.shutdown().await?;
 
     Ok(())
 }
