68be4ea867e4343828e3587ab2aeb7efaf678ec6
matheus23 <matheus23@users.noreply.github.com>
2026-08-21 12:48:06 +0000

    deploy: ae3a934a8720939d4a2a98ed184b331797645f4d

--- pr/105/docs/trait.impl/iroh_tickets/trait.Ticket.js
+++ pr/105/docs/trait.impl/iroh_tickets/trait.Ticket.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["iroh_services",[["impl Ticket for <a class=\"struct\" href=\"iroh_services/api_secret/struct.ApiSecret.html\" title=\"struct iroh_services::api_secret::ApiSecret\">ApiSecret</a>",0]]]]);
+    const implementors = Object.fromEntries([["iroh_services_proto",[["impl Ticket for <a class=\"struct\" href=\"iroh_services_proto/api_secret/struct.ApiSecret.html\" title=\"struct iroh_services_proto::api_secret::ApiSecret\">ApiSecret</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[185]}
+//{"start":59,"fragment_lengths":[203]}
