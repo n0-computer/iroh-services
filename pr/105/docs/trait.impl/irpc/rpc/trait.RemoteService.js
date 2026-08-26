d82efcf149914cbac837a2d2f91278a0623c8674
matheus23 <matheus23@users.noreply.github.com>
2026-08-26 07:11:15 +0000

    deploy: 22e1a36d279ccf404629117c8708fcb882062e77

--- pr/105/docs/trait.impl/irpc/rpc/trait.RemoteService.js
+++ pr/105/docs/trait.impl/irpc/rpc/trait.RemoteService.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["iroh_services_proto",[["impl RemoteService for <a class=\"enum\" href=\"iroh_services_proto/protocol/enum.ClientHostProtocol.html\" title=\"enum iroh_services_proto::protocol::ClientHostProtocol\">ClientHostProtocol</a>",0],["impl RemoteService for <a class=\"enum\" href=\"iroh_services_proto/protocol/enum.IrohServicesProtocol.html\" title=\"enum iroh_services_proto::protocol::IrohServicesProtocol\">IrohServicesProtocol</a>",0]]]]);
+    const implementors = Object.fromEntries([["iroh_services_proto",[["impl RemoteService for <a class=\"enum\" href=\"iroh_services_proto/enum.ClientHostProtocol.html\" title=\"enum iroh_services_proto::ClientHostProtocol\">ClientHostProtocol</a>",0],["impl RemoteService for <a class=\"enum\" href=\"iroh_services_proto/enum.IrohServicesProtocol.html\" title=\"enum iroh_services_proto::IrohServicesProtocol\">IrohServicesProtocol</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[435]}
+//{"start":59,"fragment_lengths":[397]}
