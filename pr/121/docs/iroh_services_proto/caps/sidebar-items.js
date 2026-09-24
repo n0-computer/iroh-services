a9359f0b75396819889847cc2c58014458c7862c
Arqu <Arqu@users.noreply.github.com>
2026-09-24 06:44:11 +0000

    deploy: 79175ac5c3f9343586b8cf2634ad97ebd56be17a

---
+++ pr/121/docs/iroh_services_proto/caps/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Cap","Caps","MetricsCap","NetDiagnosticsCap","RelayCap"],"struct":["CapSet"]};
---
+++ pr/121/docs/trait.impl/iroh/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["iroh_services",[["impl ProtocolHandler for <a class=\"struct\" href=\"iroh_services/struct.ClientHost.html\" title=\"struct iroh_services::ClientHost\">ClientHost</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[174]}
